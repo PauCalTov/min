1555e310bb969f36bef32e58b6314d24891bf2ac
arnferjer <a18arnferjer@inspedralbes.cat>
2021-05-04 10:34:23 +0200

    angular vinculacion

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
+import { FooterComponent } from './components/homePage/footer/footer.component';
+import { AboutComponent } from './components/homePage/about/about.component';
+import { ContactComponent } from './components/homePage/contact/contact.component';
+import { FeedbackComponent } from './components/homePage/feedback/feedback.component';
+import { HomeComponent } from './components/homePage/home/home.component';
+import { MarketComponent } from './components/homePage/market/market.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    FeedbackComponent,
+    HomeComponent,
+    MarketComponent
   ],
   imports: [
     BrowserModule,
